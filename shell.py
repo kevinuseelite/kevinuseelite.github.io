183be0ab85c44fa16ab16738cda5fcff5eec8be2
kevinuseelite <kramos@iuseelite.com>
2023-03-31 16:16:52 -0700

    Signed-off-by: kevinuseelite <kramos@iuseelite.com>

--- shell.py
+++ shell.py
@@ -1,30 +1,33 @@
 import subprocess
 import tqdm
 import time
+import logging
 from tqdm import tqdm
+
+logging.basicConfig(filename='myapp.log', level=logging.INFO)
 
 if __name__ == '__main__':
     output_file = "C:/Users/kramos/Desktop/kevinuseelite.github.io/output.pdf"
-    
+
     try:
         process = subprocess.Popen(["python", "app.py", ">", output_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     except (OSError, subprocess.SubprocessError) as e:
-        print(f"Error: {e}")
+        logging.error(f"Error: {e}")
         exit(1)
 
     with tqdm(total=100, desc="Converting to PDF", unit="%", unit_scale=True) as progress:
         for i in range(100):
             progress.update(1)
         time.sleep(0.000001)
-    
+
     # Wait for the subprocess to finish
     try:
         stdout, stderr = process.communicate(timeout=10)
     except subprocess.TimeoutExpired:
         process.kill()
-        print("Error: Subprocess timed out.")
+        logging.error("Error: Subprocess timed out.")
         exit(1)
-    
+
     if process.returncode != 0:
-        print(f"Error: Subprocess failed with code {process.returncode}.")
+        logging.error(f"Error: Subprocess failed with code {process.returncode}.")
         exit(1)
