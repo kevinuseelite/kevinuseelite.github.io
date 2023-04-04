a472f417e7ef98b46d3a3fe2f67a162818578f56
kevinuseelite <kramos@iuseelite.com>
2023-04-04 15:27:55 -0700

    Signed-off-by: kevinuseelite <kramos@iuseelite.com>

--- shell.py
+++ shell.py
@@ -3,15 +3,24 @@ import time
 import logging
 import os
 import subprocess
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from tqdm import tqdm
 import var as v
 import logging.handlers
+import pymysql
+import warnings 
+
 
 start_time = time.time()
 
 # create logger
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
+
+with warnings.catch_warnings():
+    warnings.simplefilter(action='ignore', category=FutureWarning)
+    
 
 # create a rotating file handler
 handler = logging.handlers.RotatingFileHandler(v.logger, maxBytes=1024*1024, backupCount=5)
@@ -23,38 +32,56 @@ handler.setFormatter(formatter)
 # add the handlers to the logger
 logger.addHandler(handler)
 
+# Extract the credentials from the list.
+try:
+    conn = pymysql.connect(
+        host =      'iuseeliteparts.cswkkvpqnjji.us-east-1.rds.amazonaws.com',
+        user =      'partmaster',
+        password =  'Partmaster123Partmaster123',
+        port=       3306,
+        cursorclass=pymysql.cursors.DictCursor
 
-x = 1
-if __name__ == '__main__':
-    # Check if the output file already exists
-    while x < 35000:
-        print('instance:', x)
-        if os.path.exists(v.output_file):
-            logger.warning("Output file already exists and will be overwritten.")
+  )
+except pymysql.err.OperationalError as e:
+    if e.args[0] == 1044:
+        logger.error("Access denied for user")
+    elif e.args[0] == 1045:
+        logger.error("Invalid username or password")
+    elif e.args[0] == 2003:
+        logger.error("Could not connect to MySQL server")
+    else:
+        logger.error("Error connecting to MySQL: %s", str(e))
+    exit()
+except pymysql.Error as e:
+    logger.error("Error connecting to MySQL: %s", str(e))
+    exit()
 
-        try:
-            process = subprocess.Popen(["python", "app.py", ">", v.output_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-            with tqdm(total=100, desc="Converting to PDF", unit="%", unit_scale=True) as progress:
-                for i in range(100):
-                    progress.update(1)
-            # Wait for the subprocess to finish
-            stdout, stderr = process.communicate(timeout=10)
-            if process.returncode != 0:
-                logger.error(f"Error: Subprocess failed with code {process.returncode}.")
-                logging.error(f"stdout: {process.stdout.read().decode()}")
-                logging.error(f"stderr: {process.stderr.read().decode()}")
-                exit(1)
-            else:
-                # Check if the output file has been created successfully
-                logger.info(f"Output file {v.output_file} created successfully.")
-        except (OSError, subprocess.SubprocessError, subprocess.TimeoutExpired) as e:
-            logger.error(f"Error: {e}")
-            logging.error(f"stdout: {process.stdout.read().decode()}")
-            logging.error(f"stderr: {process.stderr.read().decode()}")
-            exit(1)
-        x+=1
+# Check if the output file already exists
+if os.path.exists(v.output_file):
+    logger.warning("Output file already exists and will be overwritten.")
 
-    end_time = time.time()
-    runtime = end_time - start_time
-    logging.info(f"Script execution completed in {runtime:.2f} seconds.")
-    print(f"Script execution completed in {runtime:.2f} seconds.")
+try:
+    process = subprocess.Popen([v.shell_in, v.shell_out, ">", v.output_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    with tqdm(total=100, desc="Converting to PDF", unit="%", unit_scale=True) as progress:
+        for i in range(100):
+            progress.update(1)
+    # Wait for the subprocess to finish
+    stdout, stderr = process.communicate(timeout=10)
+    if process.returncode != 0:
+        logger.error(f"Error: Subprocess failed with code {process.returncode}.")
+        logging.error(f"stdout: {process.stdout.read().decode()}")
+        logging.error(f"stderr: {process.stderr.read().decode()}")
+        exit(1)
+    else:
+        # Check if the output file has been created successfully
+        logger.info(f"Output file {v.output_file} created successfully.")
+except (OSError, subprocess.SubprocessError, subprocess.TimeoutExpired) as e:
+    logger.error(f"Error: {e}")
+    logging.error(f"stdout: {process.stdout.read().decode()}")
+    logging.error(f"stderr: {process.stderr.read().decode()}")
+    exit(1)
+
+end_time = time.time()
+runtime = end_time - start_time
+logging.info(f"Script execution completed in {runtime:.2f} seconds.")
+print(f"Script execution completed in {runtime:.2f} seconds.")
--- var.py
+++ var.py
@@ -1,9 +1,35 @@
+from distutils.cmd import Command
 import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-# set variables
+#set variables
 output_file = "C:/Users/kramos/Desktop/kevinuseelite.github.io/output.pdf"
 base_url = 'https://kevinuseelite.github.io/'
 part_temp = 'index.html'
 platform = 'win32'
 cli_jar = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'pdfreactor.jar')
 logger = 'myapp.log'
+shell_in = 'python'
+shell_out = 'app.py'
+creds = 'google_creds.txt'
+
+# Variables to grab the images
+img_dir = r"C:\Users\kramos\Desktop\kevinuseelite.github.io\images"
+image_extensions = [".jpg", ".jpeg", ".png", ".gif", ".bmp"]
+num_images = 0
+img_paths = []
+
+#values to store the database creds
+lines = []
+with open(creds) as f:
+    lines = f.read().splitlines()
+
+
+#parses the image for the \dir
+for filename in os.listdir(img_dir):
+    if any(filename.lower().endswith(ext) for ext in image_extensions):
+        # Get the full path to the image file
+        file_path = os.path.join(img_dir, filename)
+        # Append the image file path to the list
+        img_paths.append(file_path)
