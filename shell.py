15616dd3ff7ee98b9d504be8cb050afe214c6164
kevinuseelite <kramos@iuseelite.com>
2023-04-04 17:16:26 -0700

    Signed-off-by: kevinuseelite <kramos@iuseelite.com>

--- shell.py
+++ shell.py
@@ -10,21 +10,21 @@ import var as v
 import logging.handlers
 import pymysql
 import warnings 
+from django.http import HttpResponse
+from django.shortcuts import render
 
 
 start_time = time.time()
-
 # create logger
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 with warnings.catch_warnings():
     warnings.simplefilter(action='ignore', category=FutureWarning)
-    
 
 # create a rotating file handler
 handler = logging.handlers.RotatingFileHandler(v.logger, maxBytes=1024*1024, backupCount=5)
-handler.setLevel(logging.INFO)
+handler.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
@@ -35,13 +35,15 @@ logger.addHandler(handler)
 # Extract the credentials from the list.
 try:
     conn = pymysql.connect(
-        host =      'iuseeliteparts.cswkkvpqnjji.us-east-1.rds.amazonaws.com',
-        user =      'partmaster',
-        password =  'Partmaster123Partmaster123',
-        port=       3306,
-        cursorclass=pymysql.cursors.DictCursor
+        host =          'iuseeliteparts.cswkkvpqnjji.us-east-1.rds.amazonaws.com',
+        user =          'partmaster',
+        password =      'Partmaster123Partmaster123',
+        port=           3306,
+        cursorclass=    pymysql.cursors.DictCursor
 
   )
+    logger.info("Successfully connected to MySQL server")
+
 except pymysql.err.OperationalError as e:
     if e.args[0] == 1044:
         logger.error("Access denied for user")
@@ -60,26 +62,29 @@ except pymysql.Error as e:
 if os.path.exists(v.output_file):
     logger.warning("Output file already exists and will be overwritten.")
 
-try:
-    process = subprocess.Popen([v.shell_in, v.shell_out, ">", v.output_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    with tqdm(total=100, desc="Converting to PDF", unit="%", unit_scale=True) as progress:
-        for i in range(100):
-            progress.update(1)
-    # Wait for the subprocess to finish
-    stdout, stderr = process.communicate(timeout=10)
-    if process.returncode != 0:
-        logger.error(f"Error: Subprocess failed with code {process.returncode}.")
+def run_script():
+    try:
+        process = subprocess.Popen([v.shell_in, v.shell_out, ">", v.output_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        with tqdm(total=100, desc="Converting to PDF", unit="%", unit_scale=True) as progress:
+            for i in range(100):
+                progress.update(1)
+        # Wait for the subprocess to finish
+        stdout, stderr = process.communicate(timeout=10)
+        if process.returncode != 0:
+            logger.error(f"Error: Subprocess failed with code {process.returncode}.")
+            logging.error(f"stdout: {process.stdout.read().decode()}")
+            logging.error(f"stderr: {process.stderr.read().decode()}")
+            exit(1)
+        else:
+            # Check if the output file has been created successfully
+            logger.info(f"Output file {v.output_file} created successfully.")
+    except (OSError, subprocess.SubprocessError, subprocess.TimeoutExpired) as e:
+        logger.error(f"Error: {e}")
         logging.error(f"stdout: {process.stdout.read().decode()}")
         logging.error(f"stderr: {process.stderr.read().decode()}")
         exit(1)
-    else:
-        # Check if the output file has been created successfully
-        logger.info(f"Output file {v.output_file} created successfully.")
-except (OSError, subprocess.SubprocessError, subprocess.TimeoutExpired) as e:
-    logger.error(f"Error: {e}")
-    logging.error(f"stdout: {process.stdout.read().decode()}")
-    logging.error(f"stderr: {process.stderr.read().decode()}")
-    exit(1)
+
+run_script()
 
 end_time = time.time()
 runtime = end_time - start_time
